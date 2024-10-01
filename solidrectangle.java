f0dc4690b597111bda193e4cf2f54c5676961761
Soumya007-dev <soumyadr2002@gmail.com>
2024-10-01 11:00:53 +0530

    Add files via upload

---
+++ solidrectangle.java
@@ -0,0 +1,19 @@
+public class solidrectangle {
+    public static void main(String[] args) {
+        //print a solid rectangle with 4rows and 5 columns
+       // rows=outer loop, columns=inner loop
+        int n=4;
+        int m=5;
+
+for( int i=1;i<=n;i++){
+   for (int j=1;j<=m;j++) {
+    System.out.print("$");
+       
+   }
+   System.out.println();
+}
+
+
+
+    }
+}
